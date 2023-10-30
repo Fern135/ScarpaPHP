30aff2dd3e4f05336ddd56fc3a6e53259b7c3f15
dante.camblor@gmail.com <dante.camblor@gmail.com>
2023-10-30 00:31:05 -0400

    made some more progress and attempted to make a custom growl message

--- changeLog/bin/js/docs.js
+++ changeLog/bin/js/docs.js
@@ -1,3 +1,5 @@
+import Growl from "../core/js/notification/notification.js";
+
 window.onload = Load();
 
 async function Load(){
@@ -10,5 +12,10 @@ async function Load(){
     } catch (error) {
         console.error(`Error: ${error.toString()}`);
     }
+}
+
+
+function question(){
+    Growl("Private methods will not be listed.");
 }
 
