1abcabfeb3652bd4d1c51f3f66ae69f29404233e
dante.camblor@gmail.com <dante.camblor@gmail.com>
2024-05-29 16:25:15 -0400

    added put and delete methods in the Router

--- core/Router/Router.php
+++ core/Router/Router.php
@@ -6,11 +6,19 @@ class Router {
     protected $routes = [];
 
     public function get($uri, $controllerAction) {
-        $this->routes['GET'][$uri] = $controllerAction;
+        $this->routes["GET"][$uri] = $controllerAction;
     }
 
     public function post($uri, $controllerAction) {
-        $this->routes['POST'][$uri] = $controllerAction;
+        $this->routes["POST"][$uri] = $controllerAction;
+    }
+
+    public function put($uri, $controllerAction){
+        $this->routes["PUT"][$uri] = $controllerAction;
+    }
+
+    public function delete($uri, $controllerAction){
+        $this->routes["DELETE"][$uri] = $controllerAction;
     }
 
     public function route($uri, $requestMethod) {
